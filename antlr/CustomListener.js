4dd2f6eaf202edb36ecf2a36bba09dbf66bb19aa
AkitaOnRails <fabioakita@gmail.com>
2022-04-23 15:21:31 -0300

    finally fixed dependencies and all the old JS vs new JS mess created by the generators and the blog post tutorial

--- antlr/CustomListener.js
+++ antlr/CustomListener.js
@@ -1,17 +1,14 @@
 import SqlBaseListener from './SqlBaseListener.js'
 
-export var CustomListener = function(result) {
-  this.result = result;
-  SqlBaseListener.call(this); // inherit default listener
-  return this;
-};
+export class CustomListener extends SqlBaseListener {
+  constructor(result) {
+    super();
+    this.result = result;
+  }
 
-// continue inheriting default listener
-CustomListener.prototype = Object.create(SqlBaseListener.prototype);
-CustomListener.prototype.constructor = CustomListener;
-
-// override default listener behavior
-CustomListener.prototype.exitStatement = function(ctx) {
-  console.log(ctx)
-  this.result.push([ctx.start.start, ctx.stop.stop]); // storing the start and stop indices of every statement
-};
+  // override default listener behavior
+  exitStatement = function(ctx) {
+    console.log(ctx)
+    this.result.push([ctx.start.start, ctx.stop.stop]); // storing the start and stop indices of every statement
+  }
+}
--- antlr/SplitQueries.mjs
+++ antlr/SplitQueries.mjs
@@ -1,17 +1,19 @@
-import * as antlr4 from 'antlr4';
-import {SqlBaseLexer} from "./SqlBaseLexer.js"
-import {SqlBaseParser} from "./SqlBaseParser.js"
-import {CustomListener} from "./CustomListener.js"
+"use strict";
+import {default as antlr4} from 'antlr4';
+import {SqlBaseLexer} from "./SqlBaseLexer.js";
+import {SqlBaseParser} from "./SqlBaseParser.js";
+import {CustomListener} from "./CustomListener.js";
 export var SplitQueries = function () {
-    var chars = new antlr4.InputStream("SELECT name, age FROM users WHERE age > 20 AND city = 'Gotham'");
+    var input = "SELECT name, age FROM users WHERE age > 20 AND city = 'Gotham'";
+    var chars = new antlr4.InputStream(input);
     var lexer = new SqlBaseLexer(chars);
     var tokens = new antlr4.CommonTokenStream(lexer);
     var parser = new SqlBaseParser(tokens);
     parser.buildParseTrees = true;
-    var tree = parser.queriesText(); // the tree upon which I'll walk
+    var tree = parser.statement(); // the tree upon which I'll walk
     var result = [];
     var listener = new CustomListener(result); // custom listener builds on the empty results array
     antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
     return listener.result; // this function returns the start and stop indices.
 };
-SplitQueries()
+SplitQueries();
