2c175e1af196479a949937d62088eb08d310555d
Lucas <lucas_gs_lima@hotmail.com>
2022-04-26 18:05:39 -0300

    Adding a http server reading 26422 port, and modifed stupid.mjs to serve listener.mjs

--- stupid.mjs
+++ stupid.mjs
@@ -1,4 +1,4 @@
-import * as fs from 'fs';
+//import * as fs from 'fs';
 import {SplitQueries} from './parser/SplitQueries.mjs';
 import {select,from,outerJoin,innerJoin,orderBy,groupBy,insert,deleteFrom,deleteId,
     updateFrom,updateId,count,database} from './lib/fake-sql.mjs';
@@ -7,7 +7,7 @@ import {heroes} from './lib/heroes-database.mjs';
 database['users'] = users
 database['heroes'] = heroes
 
-const readScriptFromFile = (file) => {
+/*const readScriptFromFile = (file) => {
     return new Promise((resolve, reject) => {
         fs.readFile(file, 'utf8', (err, data) => {
             if (err) {
@@ -19,14 +19,25 @@ const readScriptFromFile = (file) => {
     })
 }
 
+
+
+const script = await readScriptFromFile('test.sql');
+const parsed = parseScriptWithSplitQueries(script);
+*/
+
 const parseScriptWithSplitQueries = (script) => {
     return SplitQueries(script)
 }
 
-const script = await readScriptFromFile('test.sql');
-const parsed = parseScriptWithSplitQueries(script);
-parsed.forEach(query => {
-    console.log(`running: ${query}`);
-    const result = eval(query);
-    console.table(result);
+export async function searchWithQueryValue (query){
+
+    const parsed = parseScriptWithSplitQueries(query);
+
+    await parsed.forEach(query => {
+        console.log(`running: ${query}`);
+        const result =  eval(query)
+        console.log(result);
+        return result    
 })
+
+}
