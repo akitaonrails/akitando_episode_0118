365b43a3b83c17048ec59486d1416c09ca93a93d
AkitaOnRails <fabioakita@gmail.com>
2022-05-07 21:35:58 -0300

    fixing test.sql

--- test.sql
+++ test.sql
@@ -4,5 +4,5 @@ SELECT name,age FROM users where users.age between 20 and 30;
 SELECT name FROM users where users.age > 40 ORDER BY name DESC;
 INSERT INTO users (name,age,city) VALUES ('Katar Hol', 35, 'Thanagar');
 SELECT name,age FROM users where users.city = 'Thanagar';
-DELETE FROM users where id = 38; /* hard coded, didn't implement a way to get last inserted ID */
+DELETE FROM users where users.id = 38; /* hard coded, didn't implement a way to get last inserted ID */
 SELECT name,age FROM users where users.city = 'Thanagar';
